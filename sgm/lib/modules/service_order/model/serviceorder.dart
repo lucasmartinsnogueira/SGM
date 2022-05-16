157a434528590f82175de404be82e7ef120f5051
Lucas Martins <lucasmartinsnogueira13579@outlook.com>
2022-05-16 20:31:36 -0300

    Esboço classe OSs.

---
+++ sgm/lib/modules/service_order/model/serviceorder.dart
@@ -0,0 +1,15 @@
+class ServiceOrder{
+  
+  String? title;
+  //mecanicos
+  int? cavalo;
+  int? carreta;
+  String? description;
+  bool? done;
+  bool? stock;
+  bool? igm;
+  DateTime? data;
+ 
+ ServiceOrder(String title, int cavalo, int carreta, String description, bool done, bool stock, bool igm, DateTime data);
+}
+
